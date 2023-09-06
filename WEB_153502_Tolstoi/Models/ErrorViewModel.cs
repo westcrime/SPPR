f93cf2f7d5146de8fe13d8872f4f6ee1e2441deb
westcrime <wesstcrime77@rambler.com>
2023-09-06 11:14:47 +0300

    added lab1

---
+++ WEB_153502_Tolstoi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153502_Tolstoi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
