0ac8b0ca35509cf3e0eeceb0d31ff38cc8e24955
westcrime <wesstcrime77@rambler.com>
2023-09-08 13:38:51 +0300

    working on lab2

--- WEB_153502_Tolstoi/Controllers/HomeController.cs
+++ WEB_153502_Tolstoi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WEB_153502_Tolstoi.Models;
 
 namespace WEB_153502_Tolstoi.Controllers
 {
@@ -6,7 +7,26 @@ namespace WEB_153502_Tolstoi.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            ViewData["Title"] = "Лабораторная работа 2";
+
+            ListDemo item1 = new ListDemo();
+            item1.Id = 1;
+            item1.Name = "Item 1";
+
+            ListDemo item2 = new ListDemo();
+            item1.Id = 2;
+            item1.Name = "Item 2";
+
+            ListDemo item3 = new ListDemo();
+            item1.Id = 3;
+            item1.Name = "Item 3";
+
+            List<ListDemo> list = new List<ListDemo>();
+            list.Add(item1);
+            list.Add(item2);
+            list.Add(item3);
+
+            return View(list);
         }
     }
 }
