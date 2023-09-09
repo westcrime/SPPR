1e3909d551c7eebcf55f9c91aff1671aeece06eb
westcrime <wesstcrime77@rambler.com>
2023-09-09 20:10:28 +0300

    added 2 lab

---
+++ WEB_153502_Tolstoi/Components/CartViewComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace WEB_153502_Tolstoi.Components
+{
+    public class CartViewComponent
+    {
+        public string Invoke()
+        {
+            return "00,0";
+        }
+    }
+}
--- WEB_153502_Tolstoi/Controllers/HomeController.cs
+++ WEB_153502_Tolstoi/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WEB_153502_Tolstoi.Models;
 
 namespace WEB_153502_Tolstoi.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             ViewData["Title"] = "Лабораторная работа 2";
@@ -14,19 +16,23 @@ namespace WEB_153502_Tolstoi.Controllers
             item1.Name = "Item 1";
 
             ListDemo item2 = new ListDemo();
-            item1.Id = 2;
-            item1.Name = "Item 2";
+            item2.Id = 2;
+            item2.Name = "Item 2";
 
             ListDemo item3 = new ListDemo();
-            item1.Id = 3;
-            item1.Name = "Item 3";
+            item3.Id = 3;
+            item3.Name = "Item 3";
 
-            List<ListDemo> list = new List<ListDemo>();
-            list.Add(item1);
-            list.Add(item2);
-            list.Add(item3);
+            List<ListDemo> list = new List<ListDemo>
+            {
+                item1,
+                item2,
+                item3
+            };
 
-            return View(list);
+            var MyList = new SelectList(list, nameof(ListDemo.Id), nameof(ListDemo.Name));
+
+            return View(MyList);
         }
     }
 }
